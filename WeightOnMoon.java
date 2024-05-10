92297e08aa72760e1845169e66178ec180df97f7
Ruslan <ruslanmarlinskiy@gmail.com>
2024-05-11 01:12:37 +0300

    Add files via upload

---
+++ WeightOnMoon.java
@@ -0,0 +1,8 @@
+public class WeightOnMoon {
+    public static void main(String[] args) {
+
+        double myWeight = 80;
+        double myWeightOnAMoon = myWeight * 0.17;
+        System.out.println(myWeightOnAMoon);
+    }
+}
